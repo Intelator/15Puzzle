0bf7e486ab440827ee5f258e8398d2d5f4574ca4
Intelator <artem09.07.91@mail.ru>
2015-10-21 08:40:28 +0300

    Initial commit

---
+++ src/puzzle_core/Engine.java
@@ -0,0 +1,17 @@
+package puzzle_core;
+
+import java.util.List;
+
+/**
+ * Created by User on 21.10.2015.
+ */
+public interface Engine {
+    List<Integer> fields = null;
+
+    public void createField(int quantity);
+    public boolean hasSolvation(List<Integer> fields);
+    public boolean isFinished(List<Integer> fields);
+    public void moveTile(int direction);
+    public List<Integer> getValues();
+
+}
---
+++ src/puzzle_core/EngineImpl.java
@@ -0,0 +1,15 @@
+package puzzle_core;
+
+import java.util.ArrayList;
+
+/**
+ * Created by User on 21.10.2015.
+ */
+public class EngineImpl implements Engine {
+    private ArrayList<Integer> fields;
+
+    @Override
+    public void createField(int quantity) {
+        
+    }
+}
