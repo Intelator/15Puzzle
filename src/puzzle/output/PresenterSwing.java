e7cd3a1ca6f41e2997110508cafe90ca9ed76b04
Intelator <artem09.07.91@mail.ru>
2015-10-28 09:00:54 +0300

    GUI implementation continued

--- src/puzzle/output/PresenterSwing.java
+++ src/puzzle/output/PresenterSwing.java
@@ -1,16 +1,25 @@
 package puzzle.output;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.List;
 
 /**
  * Created by User on 27.10.2015.
  */
 public class PresenterSwing extends Presenter {
+    JFrame frame = null;
+
     public PresenterSwing() {
         super();
-        JFrame frame = new JFrame("15Puzzle");
-        frame.setSize(640, 480);
+        frame = new JFrame("15Puzzle");
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        frame.setSize(480, 560);
         frame.setVisible(true);
         log.info("Frame created.");
     }
@@ -22,6 +31,13 @@ public class PresenterSwing extends Presenter {
 
     @Override
     public void drawField(List<Integer> fields) {
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEtchedBorder());
+        frame.getContentPane().add(panel);
+        //TODO continue after drawTile
+    }
 
+    private void drawTile(int ver, int hor, int value) {
+        //TODO implement
     }
 }
