51bd5c4ba7a41c78f2597287f70f0c905a3c459d
Intelator <artem09.07.91@mail.ru>
2015-11-02 00:07:02 +0300

    Some TODO added

--- src/puzzle/output/Presenter.java
+++ src/puzzle/output/Presenter.java
@@ -14,7 +14,7 @@ public abstract class Presenter {
 
     Presenter() {
         try {
-            FileHandler fh = new FileHandler("D:\\log\\log.txt");
+            FileHandler fh = new FileHandler("D:\\log\\log.txt"); //TODO Needs to fix IOExp
             log.addHandler(fh);
 
         } catch (SecurityException e) {
