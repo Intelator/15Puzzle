2a3074d3ae8d1a3621799491c30b3180a86e1989
Intelator <artem09.07.91@mail.ru>
2015-11-13 09:18:10 +0300

    SpringFramework added, does work

--- src/puzzle/output/PresenterSwing.java
+++ src/puzzle/output/PresenterSwing.java
@@ -19,21 +19,18 @@ public class PresenterSwing extends Presenter {
         super();
     }
 
+    @Override
     public void createField (List fields) {
         painter = new Painter(fields, frameWidth, frameHeight);
         painter.setBounds(0, 0, frameWidth, frameHeight);
+
         frame = new JFrame("15Puzzle");
-//        frame.addWindowListener(new WindowAdapter() {
-//            public void windowClosing(WindowEvent e) {
-//                System.exit(0);
-//            }
-//        });
         frame.setSize(frameWidth, frameHeight);
         frame.getContentPane().setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(painter);
-        //frame.pack();
         frame.setVisible(true);
+
         drawField(fields);
         log.info("Frame created.");
     }
